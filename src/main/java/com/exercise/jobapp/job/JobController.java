849160403c54e9d4a1171cca6b3fb6327397ea54
Mustofa Adny <adniadni04@gmail.com>
2026-05-13 08:15:08 +0700

    feat: create find all jobs controller

--- src/main/java/com/exercise/jobapp/job/JobController.java
+++ src/main/java/com/exercise/jobapp/job/JobController.java
@@ -1,7 +1,17 @@
 package com.exercise.jobapp.job;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class JobController {
+    private final List<Job> jobs = new ArrayList<>();
+
+    @GetMapping("/jobs")
+    public List<Job> findAll() {
+        return jobs;
+    }
 }
